fbc0b81028a1473c61f91c6accfe08cdbc4bea0b
Medet Hasan Uğurlu <77449083+MedetHasanUgurlu@users.noreply.github.com>
2024-02-10 15:22:21 +0300

    Update StarbucksCustomerManager.java

--- Week4.1/src/Concrete/StarbucksCustomerManager.java
+++ Week4.1/src/Concrete/StarbucksCustomerManager.java
@@ -5,11 +5,11 @@ import Abstract.ICustomerService;
 import Entities.Customer;
 
 public class StarbucksCustomerManager implements ICustomerService {
-	private ICustomerCheckService _customerCheckService;
+	private ICustomerCheckService customerCheckService;
 
 	public StarbucksCustomerManager(ICustomerCheckService customerCheckService) {
 		
-		_customerCheckService = customerCheckService;
+		this.customerCheckService = customerCheckService;
 		
 	}
 	
@@ -17,8 +17,8 @@ public class StarbucksCustomerManager implements ICustomerService {
 	@Override
 	public void Save(Customer customer) {
 		// TODO Auto-generated method stub
-		if(_customerCheckService.CheckIfRealPerson()) {
-			System.out.println(customer.getFirstName()+" " +customer.getLastName() +" is added");
+		if(customerCheckService.CheckIfRealPerson()) {
+			System.out.println("Name: {} Surname: {} is added",customer.getFirstName(),customer.getLastName());
 			
 		}else
 			System.out.println("Not valid user.");
